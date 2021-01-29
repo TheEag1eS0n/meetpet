e28a1f32b100fe8d8454da7604ca14df65fefd4d
rrmode <retretx@gmail.com>
2021-01-30 03:58:56 +0500

    Livewire components

---
+++ app/Http/Livewire/Page/Pets/Concrete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Page\Pets;
+
+use Livewire\Component;
+
+class Concrete extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.pets.concrete');
+    }
+}
---
+++ app/Http/Livewire/Page/Pets/Pet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Page\Pets;
+
+use Livewire\Component;
+
+class Pet extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.pets.pet');
+    }
+}
