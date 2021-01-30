d609ac9f76e211527df92696bae04974a6d34668
TheEag1eS0n <egorfaringate@gmail.com>
2021-01-30 06:21:48 +0500

    Add styling via tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,29 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ mix(path: '/css/app.css') }}">
 </head>
-<body>
+<body class="container xl:max-w-screen-xl mx-auto min-h-screen flex flex-col">
+    <div class="w-full px-7 py-5 h-24 flex justify-between items-center sticky top-0 border-b border-gray-400 bg-white z-50">
+        <a href="" class="h-10 hover:opacity-50">
+            <img src="{{ \Illuminate\Support\Facades\Storage::url('assets/MeetPetlogo.png') }}" alt="" class="h-full object-contain">
+        </a>
+        <ul class="flex justify-start h-10">
+            <li class="flex items-center">
+                <a href="" class="h-1/2 hover:opacity-50">
+                    <img src="{{ \Illuminate\Support\Facades\Storage::url("assets/find.svg") }}" alt="" class="h-full object-contain">
+                </a>
+            </li>
+            <li class="flex items-center">
+                <a href="/chat" class="h-1/2 hover:opacity-50">
+                    <img src="{{ \Illuminate\Support\Facades\Storage::url("assets/chat.svg") }}" alt="" class="h-full object-contain">
+                </a>
+            </li>
+            <li class="flex items-center">
+                <a href="/profile" class="ring-1 ring-mint rounded p-2 px-3 text-mint hover:bg-mint hover:text-white hover:opacity-50">Мой профиль</a>
+            </li>
+        </ul>
+    </div>
     {{ $slot }}
 
-    <script src="{{ mix(path: '/js/app.js') }}"></script>
+{{--    <script src="{{ mix(path: '/js/app.js') }}"></script>--}}
 </body>
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            mint: "rgba(95, 202, 163, 1)",
+        },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
